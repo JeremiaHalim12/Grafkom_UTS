0d31fca6de0bfa99d1118221b7d66fa6ffb028d3
JeremiaHalim <2072021@maranatha.ac.id>
2023-11-09 05:36:43 +0700

    final uts

--- T07_2072014_2072021/proyek web 1/index.js
+++ T07_2072014_2072021/proyek web 1/index.js
@@ -1,4 +1,3 @@
-// Function to draw the empty chart
 function drawEmptyChart() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -17,8 +16,68 @@ function drawEmptyChart() {
 
         ctx.fillStyle = 'blue';
         ctx.fillRect(x, y, barWidth, barHeight);
-        ctx.fillStyle = 'black';
-        ctx.fillText(bar.year, x + barWidth / 2, maxBarHeight - 5);
-        ctx.fillText(`Count: ${bar.count}`, x + barWidth / 2, maxBarHeight - 20);
+        // for (let i = 0; i < 500; i++){
+        //     dda_line(imageData, x, y+i, barWidth, barHeight + i, 0, 0, 255);
+        // }
+
+        const textX = x + barWidth / 2 - 50;
+
+        ctx.fillStyle = 'white';
+        ctx.fillText(bar.year, textX, maxBarHeight - 5);
+        ctx.fillStyle = 'white';
+        ctx.fillText(`Jumlah Set: ${bar.count}`, textX, maxBarHeight - 20);
+    }
+}
+
+function titik(imgData, x, y, r, g, b) {
+    let indeks = 4 * (Math.ceil(x) + (Math.ceil(y) * imgData.width));
+    imgData.data[indeks] = r;
+    imgData.data[indeks + 1] = g;
+    imgData.data[indeks + 2] = b;
+    imgData.data[indeks + 3] = 255;
+}
+
+function dda_line(imgData, x1, y1, x2, y2, r, g, b) {
+    var dx = x2 - x1;
+    var dy = y2 - y1;
+
+    if (Math.abs(dx) > Math.abs(dy)) {
+        // sumbu x
+        if (x2 > x1) {
+            // ke kanan
+            var y = y1;
+            for (var x = x1; x < x2; x++) {
+                y = y + dy / Math.abs(dx);
+                titik(imgData, x, y, r, g, b);
+            }
+        }
+        else {
+            // ke kiri
+            var y = y1;
+            for (var x = x1; x > x2; x--) {
+                y = y + dy / Math.abs(dx);
+                titik(imgData, x, y, r, g, b);
+            }
+
+        }
+    }
+    else {
+        // sumbu y
+        if (y2 > y1) {
+            // ke kanan
+            var x = x1;
+            for (var y = y1; y < y2; y++) {
+                x = x + dx / Math.abs(dy);
+                titik(imgData, x, y, r, g, b);
+            }
+        }
+        else {
+            // ke kiri
+            var x = x1;
+            for (var y = y1; y > y2; y--) {
+                x = x + dx / Math.abs(dy);
+                titik(imgData, x, y, r, g, b);
+            }
+        }
     }
 }
